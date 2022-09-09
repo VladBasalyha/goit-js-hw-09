fa552c4f298e29efc4dd988b2f0b754b05439503
VladBasalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-09-09 22:54:26 +0000

    Deploying to gh-pages from @ VladBasalyha/goit-js-hw-09@bfc0640bc7a3d139fd395d0a4e453556b4cb2999 🚀

---
+++ 01-color-switcher.5c115f11.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};t.startBtn.addEventListener("click",(function(){setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor="".concat(t)}),3e3)}))}();
+//# sourceMappingURL=01-color-switcher.5c115f11.js.map
---
+++ 01-color-switcher.6056b789.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},{startBtn:o,stopBtn:n}=t;o.addEventListener("click",(function(){setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=`${t}`}),3e3)}));
+//# sourceMappingURL=01-color-switcher.6056b789.js.map
